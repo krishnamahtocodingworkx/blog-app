78aa30b2e67b34a6f4377b468b144b624935575e
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-05-29 16:17:46 +0530

    created Login page component using meterial ui

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,38 +1,92 @@
 import * as React from "react";
-import CssBaseline from "@mui/material/CssBaseline";
-import Container from "@mui/material/Container";
-import { Button, TextField, Typography } from "@mui/material";
+import {
+  CssBaseline,
+  Container,
+  Button,
+  TextField,
+  Typography,
+  InputAdornment,
+  IconButton,
+  Link,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 import "../index.css";
 
 export default function Login() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleSubmit = () => {
+    console.log("Logging in with", { email, password });
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="sm">
         <div className="form-container">
-          <Typography variant="h5" gutterBottom component="h1" align="center">
+          <Typography variant="h5" gutterBottom align="center">
             Sign In to your account
           </Typography>
           <hr />
-          <hr />
 
-          <div className="form-field-container">
+          <form onSubmit={handleSubmit} className="form-field-container">
             <div className="form-field">
+              <Typography variant="subtitle1" gutterBottom>
+                Email Address
+              </Typography>
               <TextField
-                id="outlined-basic"
-                label="Enter email address"
+                fullWidth
+                placeholder="Enter email address"
                 variant="outlined"
+                name="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
+
             <div className="form-field">
+              <Typography variant="subtitle1" gutterBottom>
+                Password
+              </Typography>
               <TextField
-                id="outlined-basic"
-                label="Enter Password"
+                fullWidth
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter Password"
                 variant="outlined"
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton onClick={togglePasswordVisibility} edge="end">
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
               />
             </div>
-            <Button variant="contained">Login</Button>
-          </div>
+
+            <div className="form-field" style={{ textAlign: "left" }}>
+              <Link href="#" underline="hover" variant="body2">
+                Forgot password?
+              </Link>
+              <Button
+                type="submit"
+                variant="contained"
+                style={{ float: "inline-end" }}
+              >
+                Login
+              </Button>
+            </div>
+          </form>
         </div>
       </Container>
     </React.Fragment>
