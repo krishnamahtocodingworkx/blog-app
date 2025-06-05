e000e151233b537516c42c3f48f6b3a766e37c6c
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-06-05 15:26:30 +0530

    implementing redux-persist and redux logger

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,35 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import blogReducer from "./slices/blogSlice";
+import { combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; 
+import logger from "redux-logger";
+
+// Combine reducers
+const rootReducer = combineReducers({
+  auth: authReducer,
+  blog: blogReducer,
+});
+
+// Persist config
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth", "blog"], 
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    blog: blogReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, 
+    }).concat(logger),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
