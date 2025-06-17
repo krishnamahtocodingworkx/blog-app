7a001b43249f7601a9d156908fecc668c13e60a5
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-06-17 15:26:14 +0530

    deleted blogPageService from srvices folder

--- src/utils/endPoints.ts
+++ src/utils/endPoints.ts
@@ -1,7 +1,6 @@
 export const ENDPOINTS = {
   LOGIN: "/api/v1/user/login",
   BLOGS: "/api/blogs",
-  BLOGS_PAGE: "/api/blogs",
   FORGOT_PASSWORD: "/api/v1/user/resend-otp",
   OTP_VERIFICATION: "/api/v1/user/verify-email",
   RESET_PASSWORD: "/api/v1/user/reset-password",
